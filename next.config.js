db7916a287a956b67cd62b2e9f95a687e6849aac
hansan529 <hansan0529@gmail.com>
2023-08-20 23:55:31 +0900

    docs: KMDB 데이터 허용

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
+  images: {
+    domains: ['file.koreafilm.or.kr'],
+  },
+};
 module.exports = nextConfig
